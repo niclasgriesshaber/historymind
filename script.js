50b1ff50af64608dad656346e3abe6da1cfa6f22
Niclas Grießhaber <griesshaber.niclas@gmail.com>
2025-10-15 23:48:53 +0100

    Fix Full Dataset download text order and styling
    - Change text order from 'CSV Download' to 'Download CSV' and 'XLSX Download' to 'Download XLSX'
    - Remove emojis from download options for cleaner appearance
    - Maintain original styling: 'Download' in blue bold (.item-number) and format types in regular text (.item-text)
    - Improve readability and user experience for download options

--- script.js
+++ script.js
@@ -139,13 +139,13 @@ class HistoryMindApp {
                 <div class="content-page">
                     <div class="content-items">
                         <div class="content-item clickable" onclick="app.downloadCSV()">
-                            <span class="item-number">CSV</span>
-                            <span class="item-text">Download</span>
+                            <span class="item-number">Download</span>
+                            <span class="item-text">CSV</span>
                         </div>
                         
                         <div class="content-item clickable" onclick="app.downloadXLSX()">
-                            <span class="item-number">XLSX</span>
-                            <span class="item-text">Download</span>
+                            <span class="item-number">Download</span>
+                            <span class="item-text">XLSX</span>
                         </div>
                     </div>
                 </div>
